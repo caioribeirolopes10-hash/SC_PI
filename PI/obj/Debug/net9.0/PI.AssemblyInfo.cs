27218ddb1049a59d35638cdc27e702c365d517ac
caioribeirolopes10-hash <caioribeirolopes10@gmail.com>
2026-08-13 20:44:04 -0300

    commit

--- PI/obj/Debug/net9.0/PI.AssemblyInfo.cs
+++ PI/obj/Debug/net9.0/PI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48c0edeca977676b43a51ddd00405fbff59d5d78")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9c2b9f45ac34344150786bdf3846f6f9d62b488")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
