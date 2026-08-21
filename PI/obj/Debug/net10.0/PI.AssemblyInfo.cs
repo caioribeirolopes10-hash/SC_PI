4eb0b5922a82a0b094e336957c036a937eb0f8d3
dandarah-ed9 <dandarahmagalhaes9@gmail.com>
2026-08-20 21:38:53 -0300

    PI

--- PI/obj/Debug/net10.0/PI.AssemblyInfo.cs
+++ PI/obj/Debug/net10.0/PI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbb78cd443ab62114fcbf2f60a54c3294fdb2190")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c2b40032a1212f01034c1e541b08f99847675ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("PI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
